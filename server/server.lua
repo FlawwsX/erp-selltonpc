463625d9feb0700aebcc25cb3395c0ff6a8e742f
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-02-18 21:09:11 +0000

    Fixing "minus in items"

--- server/server.lua
+++ server/server.lua
@@ -8,9 +8,14 @@ AddEventHandler('np_selltonpc:dodeal', function()
 		local weedprice = math.random(150, 500)
 		local weedamount = math.random(1, 5)
 		TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'success', text = 'You sold ' .. weedamount .. ' weed for $' .. weedprice, length = 4000 })
-		xPlayer.removeInventoryItem('weed_pooch', weedamount)
-		local moneyamount = weedamount * weedprice
-		xPlayer.addAccountMoney('black_money', moneyamount)
+		-- Checking to see if they have enough weed to stop going negative...
+		if xPlayer.getInventoryItem('weed_pooch').count > weedamount then
+			xPlayer.removeInventoryItem('weed_pooch', weedamount)
+			local moneyamount = weedamount * weedprice
+			xPlayer.addAccountMoney('black_money', moneyamount)
+		else
+			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You do not have enough weed to sell.', length = 5000, })
+		end
 	end
 end)
 
