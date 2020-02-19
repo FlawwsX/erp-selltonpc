724b8e8cc1d889022247ab0dee4113d0eb480f06
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-02-19 15:13:02 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.IgnorePolice = false -- Enable to allow police job to sell drugs.
 Config.DistanceCheck = true -- Enable to check if player has walked away from the ped or the ped has walked away.
 Config.EnableAnimation = true -- Weed sell animation
-Config.NumberOfCops = 1
+Config.NumberOfCops = 1 -- Set to 0 to disable.
 
 -- ## Weed ## --
 Config.EnableWeed = true
