aa1d258e3abc65a0d796119a10f67e83b466bca9
FlawwsX <55849405+FlawwsX@users.noreply.github.com>
2020-07-10 09:23:26 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,7 @@ description 'Optimized sell drugs to NPC'
 version '1.0.4'
 
 server_scripts {
-    '@async/async.lua',
     '@mysql-async/lib/MySQL.lua',
-    
     'server/server.lua',
     'config.lua'
 }
